278274c0662787e5020b1942e8036a593aa04bcf
DmitryPoverov <did.ink@mail.ru>
2021-12-14 20:08:14 +0300

    Corrected Calculator Task

--- src/test/java/com/epam/task1/CalculatorTest.java
+++ src/test/java/com/epam/task1/CalculatorTest.java
@@ -6,69 +6,95 @@ import org.junit.Test;
 public class CalculatorTest {
 
     private static final double DELTA = 0.00000001;
-    private Calculator calculator = new Calculator();
+
 
     @Test
-    public void shouldAddAndReturnThreeWhenOneAndTwo() {
-        double result = calculator.add(1, 2);
-        Assert.assertEquals(result, 3, DELTA);
-    }
-
-    @Test
-    public void shouldAddAndReturnEightWhenSixAndTwo() {
+    public void testShouldAddAndReturnEightWhenSixAndTwo() {
+        //given
+        Calculator calculator = new Calculator();
+        //when
         double result = calculator.add(6, 2);
+        //then
         Assert.assertEquals(result, 8, DELTA);
     }
 
     @Test
-    public void shouldAddAndReturnMinusThreeWhenMinusOneAndMinusTwo() {
-        double result = calculator.add(-1, -2);
-        Assert.assertEquals(result, -3, DELTA);
-    }
-    @Test
-    public void shouldAddAndReturnMinusThreeWhenMinusThreeAndMinusTwo() {
+    public void testShouldAddAndReturnMinusThreeWhenMinusThreeAndMinusTwo() {
+        //given
+        Calculator calculator = new Calculator();
+        //when
         double result = calculator.add(-3, -2);
+        //then
         Assert.assertEquals(result, -5, DELTA);
     }
 
     @Test
-    public void shouldSubtractAndReturnOneWhenThreeAndTwo() {
+    public void testShouldSubtractAndReturnOneWhenThreeAndTwo() {
+        //given
+        Calculator calculator = new Calculator();
+        //when
         double result = calculator.subtract(3, 2);
+        //then
         Assert.assertEquals(result, 1, DELTA);
     }
 
     @Test
-    public void shouldSubtractAndReturnOneWhenThirteenAndTen() {
+    public void testShouldSubtractAndReturnOneWhenThirteenAndTen() {
+        //given
+        Calculator calculator = new Calculator();
+        //when
         double result = calculator.subtract(13, 10);
+        //then
         Assert.assertEquals(result, 3, DELTA);
     }
 
     @Test
-    public void shouldMultiplyAndReturnSixWhenTwoAndThree() {
+    public void testShouldMultiplyAndReturnSixWhenTwoAndThree() {
+        //given
+        Calculator calculator = new Calculator();
+        //when
         double result = calculator.multiply(2, 3);
+        //then
         Assert.assertEquals(result, 6, DELTA);
     }
 
     @Test
-    public void shouldMultiplyAndReturnSixteenWhenTwoAndEight() {
+    public void testShouldMultiplyAndReturnSixteenWhenTwoAndEight() {
+        //given
+        Calculator calculator = new Calculator();
+        //when
         double result = calculator.multiply(2, 8);
+        //then
         Assert.assertEquals(result, 16, DELTA);
     }
 
     @Test
-    public void shouldDivideWhenInputIsCorrectAndReturnFiveWhenTenAndTwo() {
+    public void testShouldDivideWhenInputIsCorrectAndReturnFiveWhenTenAndTwo() {
+        //given
+        Calculator calculator = new Calculator();
+        //when
         double result = calculator.divide(10, 2);
+        //then
         Assert.assertEquals(result, 5, DELTA);
     }
 
     @Test
-    public void shouldDivideWhenInputIsCorrectAndReturnMinusThreeWhenTwelveAndMinusFour() {
+    public void testShouldDivideWhenInputIsCorrectAndReturnMinusThreeWhenTwelveAndMinusFour() {
+        //given
+        Calculator calculator = new Calculator();
+        //when
         double result = calculator.divide(-12, 4);
+        //then
         Assert.assertEquals(result, -3, DELTA);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void shouldThrowExceptionWhenSecondIs0() {
+    public void testShouldThrowExceptionWhenSecondOperandIs0() {
+        //given
+        Calculator calculator = new Calculator();
+        //when
         double result = calculator.divide(4, 0);
+        //then
+        //I don't know what's the "then" here.
     }
 }
