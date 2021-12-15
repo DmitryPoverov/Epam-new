1b7ff1b65a5ec151149d66401b6ead0496737a6d
DmitryPoverov <did.ink@mail.ru>
2021-12-15 10:00:18 +0300

    Corrected Calculator Task (corrected variable names)

--- src/main/java/com/epam/task1/Calculator.java
+++ src/main/java/com/epam/task1/Calculator.java
@@ -2,23 +2,23 @@ package com.epam.task1;
 
 public class Calculator {
 
-    public double add(double firstOperand, double secondOperand) {
-        return firstOperand + secondOperand;
+    public double add(double first, double second) {
+        return first + second;
     }
 
-    public double subtract(double firstOperand, double secondOperand) {
-        return firstOperand - secondOperand;
+    public double subtract(double first, double second) {
+        return first - second;
     }
 
-    public double multiply(double firstOperand, double secondOperand) {
-        return firstOperand * secondOperand;
+    public double multiply(double first, double second) {
+        return first * second;
     }
 
-    public double divide(int firstOperand, int secondOperand) {
+    public double divide(int first, int second) {
 
-        if (secondOperand == 0) {
+        if (second == 0) {
             throw new IllegalArgumentException("It's not possible dividing by 0");
         }
-        return (double)firstOperand / secondOperand;
+        return (double) first / second;
     }
 }
