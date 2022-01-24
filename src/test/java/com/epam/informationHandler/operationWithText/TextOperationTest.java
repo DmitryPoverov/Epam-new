a6d9d3d83b9f585f609c1280ec408e16b884d8ae
DmitryPoverov <did.ink@mail.ru>
2022-01-24 18:14:46 +0300

    tests were added

--- src/test/java/com/epam/informationHandler/operationWithText/TextOperationTest.java
+++ src/test/java/com/epam/informationHandler/operationWithText/TextOperationTest.java
@@ -3,12 +3,10 @@ package com.epam.informationHandler.operationWithText;
 import com.epam.informationHandler.component.Component;
 import com.epam.informationHandler.component.Composite;
 import com.epam.informationHandler.component.Lexeme;
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 
 public class TextOperationTest {
 
@@ -36,36 +34,23 @@ public class TextOperationTest {
             new Composite(Arrays.asList(COMPOSITE_PARAGRAPH_ONE, COMPOSITE_PARAGRAPH_TWO, COMPOSITE_PARAGRAPH_THREE));
 
     @Test
-    public void testSortBySentencesQuantityShouldSortSentences() throws TextException {
+    public void testSortBySentencesQuantityShouldSortSentences() {
         //given
-
-        List<Component> expectedText =
-                Arrays.asList(FOURTH_PARAGRAPH_COMPOSITE, THIRD_PARAGRAPH_COMPOSITE, SECOND_PARAGRAPH_COMPOSITE, FIRST_PARAGRAPH_COMPOSITE);
         //when
-        List<Component> actual = SERVICE.sortBySentencesQuantity(testText);
         //then
-        Assert.assertEquals(expectedText, actual);
     }
 
     @Test
-    public void testSortWordsInSentenceByLongShouldSortWords() throws TextException {
+    public void testSortWordsInSentenceByLongShouldSortWords() {
         //given
-        Component testSentence = new Composite(Arrays.asList(Lexeme.word("At"), Lexeme.word("vero"), Lexeme.word("eos")));
-        List<Component> expectedSentence = Arrays.asList(Lexeme.word("At"), Lexeme.word("eos"), Lexeme.word("vero"));
         //when
-        List<Component> actual = SERVICE.sortWordsInSentenceByLong(testSentence);
         //then
-        Assert.assertEquals(expectedSentence, actual);
     }
 
     @Test
-    public void testRestoreTextFromComponentShouldReturnRestoredText() throws TextException {
+    public void testRestoreTextFromComponentShouldReturnRestoredText() {
         //given
-        Component testText = new Composite(Arrays.asList(Lexeme.word("Lorem"), Lexeme.word("ipsum"), Lexeme.word("dolor"), Lexeme.word("sit")));
-        String expected = "Lorem ipsum dolor sit";
         //when
-        String actual = SERVICE.restoreTextFromComponent(testText);
         //then
-        Assert.assertEquals(expected, actual);
     }
 }
